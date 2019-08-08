84afaf509714751aa6d70bff43e1f4c6ef205670
george.burton <george.burton@wework.com>
2019-08-08 00:39:13 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,23 +1,23 @@
 function callMe() {
- var lyric = “maybe”;
- console.log(“I just met you...“);
- console.log(“and this is crazy..“);
- console.log(“but here’s my number..“);
- console.log(“so call me”);
+ var lyric = "maybe";
+ console.log("I just met you...");
+ console.log("and this is crazy..");
+ console.log("but here’s my number..");
+ console.log("so call me");
  return lyric;
 }
 
 
 function crazy() {
  var thisIsCrazy = function (){
-   console.log(“hey!!!“)
+   console.log("hey!!!")
  }
  thisIsCrazy();
 }
 
 function sayMyName() {
 var name;
- name ="Kristin”;
+ name ="Kristin";
  function sayMy() {
    console.log(name);
   }
